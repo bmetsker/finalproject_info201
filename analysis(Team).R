23885fe9dc4700118d47080aab6450fca6a60735
KUNPEI PENG <PENGK3@UW.EDU>
2019-11-16 22:50:22 -0800

    new updates on formating

--- analysis(Team).R
+++ analysis(Team).R
@@ -28,6 +28,22 @@ ffr_min_rate <- min(ffr_df$FEDFUNDS)
 ffr_max_rate <- max(ffr_df$FEDFUNDS)
 
 # Chart 1
+#visualizing Federal Interest Rate record of changes from 2006 to 2019
+
+Federal_rate_change <- plot_ly(
+  data = ffr_df,      
+  x = ~DATE, 
+  y = ~FEDFUNDS, 
+  type = "scatter",
+  mode = "lines",
+  alpha = .7,
+  hovertext = "FEDFUNDs"
+) %>%
+  layout(
+    title = "Federal Fund Rate 2006-2019",                
+    xaxis = list(title = "Date"),
+    yaxis = list(title = "Federal Fund Rate") 
+  )
 
 # Chart 2
 chart2_tech <- plot_ly(
@@ -45,30 +61,4 @@ chart2_tech <- plot_ly(
     yaxis = list(title = "Open rate")
   )
 
-#Code for Chart 1: 
 
-#Create a variable that represent the Federal Fund Rate Change file 
-unfiltered_ffr_df <- data.frame(read.csv("data/Effective_Feceral_Funds_Rate_07011954_10012019.csv", stringsAsFactors = FALSE))
-unfiltered_ffr_df$Year <- substr(unfiltered_ffr_df$DATE, 1, 4)
-ffr_df <- filter(unfiltered_ffr_df, Year >= 2006)
-
-library("ggplot2")
-library("plotly")
-
-#visualizing Federal Interest Rate record of changes from 1954 to 2019
-
-
-Federal_rate_change <- plot_ly(
-  data = ffr_df,      
-  x = ~DATE, 
-  y = ~FEDFUNDS, 
-  type = "scatter",
-  mode = "lines",
-  alpha = .7,
-  hovertext = "FEDFUNDs"
-) %>%
-  layout(
-    title = "Federal Interest Rate record of changes from 2006 to 2019",                
-    xaxis = list(title = "Date"),
-    yaxis = list(title = "Federal Fund Interest Rate") 
-  )
