218cee48bdafc690860dc7aa0f9e521b45eaf5d2
KUNPEI PENG <PENGK3@UW.EDU>
2019-11-18 18:30:07 -0800

    added a function that calls the graph

--- analysis(Kunpei).R
+++ analysis(Kunpei).R
@@ -1,27 +1,26 @@
-
+#Code for CHart 1: 
 
 #Create a variable that represent the Federal Fund Rate Change file 
-unfiltered_ffr_df <- data.frame(read.csv("data/Effective_Feceral_Funds_Rate_07011954_10012019.csv", stringsAsFactors = FALSE))
-unfiltered_ffr_df$Year <- substr(unfiltered_ffr_df$DATE, 1, 4)
-ffr_df <- filter(unfiltered_ffr_df, Year >= 2006)
 
 library("ggplot2")
 library("plotly")
 
 #visualizing Federal Interest Rate record of changes from 1954 to 2019
+fed_fund_plot <- function(dataset) {
 
+  Federal_rate_change <- plot_ly(
+    data = dataset,      
+    x = ~DATE, 
+    y = ~FEDFUNDS, 
+    type = "scatter",
+    mode = "lines",
+    alpha = .7,
+    hovertext = "FEDFUNDs"
+  ) %>%
+    layout(
+      title = "Federal Interest Rate record of changes from 2006 to 2019",                
+      xaxis = list(title = "Date"),
+      yaxis = list(title = "Federal Fund Interest Rate") 
+    )
+}
 
-Federal_rate_change <- plot_ly(
-  data = ffr_df,      
-  x = ~DATE, 
-  y = ~FEDFUNDS, 
-  type = "scatter",
-  mode = "lines",
-  alpha = .7,
-  hovertext = "FEDFUNDs"
-) %>%
-  layout(
-    title = "Federal Interest Rate record of changes from 2006 to 2019",                
-    xaxis = list(title = "Date"),
-    yaxis = list(title = "Federal Fund Interest Rate") 
-  )
