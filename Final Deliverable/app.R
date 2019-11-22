a29c68a41f65e8a541e9dade571439cf90fa04da
Ben <benmetsker@gmail.com>
2019-11-22 14:57:47 -0800

    draft

--- Final Deliverable/app.R
+++ Final Deliverable/app.R
@@ -1,1 +1,12 @@
+library(shiny)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(shinythemes)
+library(rsconnect)
 
+source("app_ui.R")
+source("app_server.R")
+
+
+shinyApp(ui = ui, server = server)
--- Final Deliverable/app_ui.R
+++ Final Deliverable/app_ui.R
@@ -1,1 +1,49 @@
 
+# All of the names in here are just placeholders I pulled from the last hw assignment. Feel free to change.
+
+ui<- tagList(
+  navbarPage(
+    theme = shinytheme("flatly"),
+    "Federal Funds Rate Change Analysis",
+    
+    tabPanel("Metric 1",
+             sidebarPanel(
+               selectInput('x1', 'Select X-Axis:', choices = c("state", "category"),
+                           selected = "state"),
+               selectInput('y1', 'Select Y-Axis:', choices = c( "popwhite", "popblack", "poptotal", "popamerindian", "popasian"), 
+                           selected = "poptotal"),
+             ),
+             
+             mainPanel("Racial Variance", plotOutput(outputId = "popchart"),
+             )
+          ),
+    
+    
+    tabPanel("Metric 2",
+             sidebarPanel(
+               radioButtons('x2', 'Change X-Axis:', choices = c("percollege", "percbelowpoverty", "percchildbelowpovert", "percadultpoverty", "percelderlypoverty"),
+                            selected = "percollege"),
+               radioButtons('y2', 'Change Y-Axis:', choices = c("poptotal", "popwhite", "popblack", "popamerindian", "popasian"),
+                            selected = "poptotal")
+               # Radio Buttons on different races 
+             ),
+             
+             mainPanel("College Attendace", plotOutput(outputId = "collegechart"),
+             )
+          ),
+    
+    
+    tabPanel("Metric 2",
+             sidebarPanel(
+               radioButtons('x2', 'Change X-Axis:', choices = c("percollege", "percbelowpoverty", "percchildbelowpovert", "percadultpoverty", "percelderlypoverty"),
+                            selected = "percollege"),
+               radioButtons('y2', 'Change Y-Axis:', choices = c("poptotal", "popwhite", "popblack", "popamerindian", "popasian"),
+                            selected = "poptotal")
+               # Radio Buttons on different races 
+             ),
+             
+             mainPanel("College Attendace", plotOutput(outputId = "collegechart"),
+             )
+          )
+        )
+      )
