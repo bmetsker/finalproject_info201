251310b64261889fee578b07b2a7d57f554bd02a
Ben <benmetsker@gmail.com>
2019-12-01 09:49:41 -0800

    draft

--- Final Deliverable/app_ui.R
+++ Final Deliverable/app_ui.R
@@ -1,56 +1,41 @@
 
-# All of the names in here are just placeholders I pulled from the last hw assignment. Feel free to change.
-source("storedtext.R")
+# All of the names in here are just placeholders I pulled from the last hw assignment. Feel free to change
 
 ui<- tagList(
-  navbarPage(
-    theme = shinytheme("flatly"),
-    "Federal Funds Rate Change Analysis",
-    
-    tabPanel("Introduction",
-             h1(test),
-             test_text,
-             img("lol", src = "Pictures/fc4801baa943d9526e81f45bd0473ea5.jpg")
-             ),
-    
-    tabPanel("FFR/Industry Correlation",
-             sidebarPanel(
-               selectInput('x1', 'Select X-Axis:', choices = c("state", "category"),
-                           selected = "state"),
-               selectInput('y1', 'Select Y-Axis:', choices = c( "popwhite", "popblack", "poptotal", "popamerindian", "popasian"), 
-                           selected = "poptotal"),
-             ),
-             
-             mainPanel("Racial Variance", plotOutput(outputId = "popchart"),
-             )
-          ),
-    
-    
-    tabPanel("Volume by Industry",
-             sidebarPanel(
-               radioButtons('x2', 'Change X-Axis:', choices = c("percollege", "percbelowpoverty", "percchildbelowpovert", "percadultpoverty", "percelderlypoverty"),
-                            selected = "percollege"),
-               radioButtons('y2', 'Change Y-Axis:', choices = c("poptotal", "popwhite", "popblack", "popamerindian", "popasian"),
-                            selected = "poptotal")
-               # Radio Buttons on different races 
-             ),
-             
-             mainPanel("College Attendace", plotOutput(outputId = "collegechart"),
-             )
-          ),
-    
-    
-    tabPanel("Title?",
-             sidebarPanel(
-               radioButtons('x2', 'Change X-Axis:', choices = c("percollege", "percbelowpoverty", "percchildbelowpovert", "percadultpoverty", "percelderlypoverty"),
-                            selected = "percollege"),
-               radioButtons('y2', 'Change Y-Axis:', choices = c("poptotal", "popwhite", "popblack", "popamerindian", "popasian"),
-                            selected = "poptotal")
-               # Radio Buttons on different races 
-             ),
-             
-             mainPanel("College Attendace", plotOutput(outputId = "collegechart"),
-             )
-          )
+      navbarPage(
+        theme = shinytheme("flatly"),
+        "Midwest Population Analysis",
+        
+        tabPanel("Introduction",
+                 main_title,
+                 team_members,
+                 project_explanation_title
+        ), 
+        
+        tabPanel("Race Statistics",
+                 sidebarPanel(
+                   selectInput('x1', 'Select X-Axis:', choices = c("state", "category"),
+                               selected = "state"),
+                   selectInput('y1', 'Select Y-Axis:', choices = c( "popwhite", "popblack", "poptotal", "popamerindian", "popasian"), 
+                               selected = "poptotal"),
+                 ),
+                 
+                 mainPanel("Racial Variance", plotOutput(outputId = "popchart"),
+                 )
+        ),
+        
+        
+        tabPanel("Quality of Life Statistics",
+                 sidebarPanel(
+                   radioButtons('x2', 'Change X-Axis:', choices = c("percollege", "percbelowpoverty", "percchildbelowpovert", "percadultpoverty", "percelderlypoverty"),
+                                selected = "percollege"),
+                   radioButtons('y2', 'Change Y-Axis:', choices = c("poptotal", "popwhite", "popblack", "popamerindian", "popasian"),
+                                selected = "poptotal")
+                   # Radio Buttons on different races 
+                 ),
+                 
+                 mainPanel("College Attendace", plotOutput(outputId = "collegechart"),
+                 )
         )
       )
+    )
--- Final Deliverable/storedtext.R
+++ Final Deliverable/storedtext.R
@@ -1,2 +1,3 @@
-test <- print("Title")
-test_text <- print("The stock market is doing grrrrrrrreat!")
+main_title <- print(HTML("<h1><center><font size=14> Federal Funds Rate Change Analysis </font></center></h1>"))
+team_members <- HTML("<body><center> Angel Lin, Ben Metsker, Jimmy Peng, Jonathan Chen, Mary Giang </center></body>")
+project_explanation_title <- HTML("<h3> Project Explanation </h3>")
