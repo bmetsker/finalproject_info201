bf5a8d0c87d408326500d32704b14c06ec7b9281
KUNPEI PENG <PENGK3@UW.EDU>
2019-12-02 15:14:27 -0800

    new update for Fluctuation graph

--- Final Deliverable/storedtext.R
+++ Final Deliverable/storedtext.R
@@ -1,5 +1,5 @@
 main_title <- print(HTML("<h1><center><font size=14> Federal Funds Rate Change Analysis </font></center></h1>"))
-team_members <- HTML("<body><center> Angel Lin, Ben Metsker, Jimmy Peng, Jonathan Chen, Mary Giang </center></body>")
+team_members <- HTML("<body><center> Jonathan Chen, Ben Metsker, Angel Lin, Jimmy Peng, Mary Truong </center></body>")
 project_explanation_title <- HTML("<h3> Project Explanation </h3>")
 project_explanation <- HTML("<body>
                             <br>
